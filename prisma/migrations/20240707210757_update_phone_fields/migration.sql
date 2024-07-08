6edd2dc00f5d3933ee9c4f7d3caea77eeab143fd
TusharRathi108 <tushar.rathi.tech@gmail.com>
2024-07-08 07:37:04 +0530

    Migrated to railway

--- prisma/migrations/20240707210757_update_phone_fields/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `Refer` MODIFY `ReferrerPhone` BIGINT NOT NULL,
-    MODIFY `SenderPhone` BIGINT NOT NULL;
--- prisma/migrations/20240707230808_update_phone_fields_to_string/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `Refer` MODIFY `ReferrerPhone` VARCHAR(10) NOT NULL,
-    MODIFY `SenderPhone` VARCHAR(10) NOT NULL;
